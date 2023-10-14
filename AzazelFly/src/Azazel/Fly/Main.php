912bb6fe117dc74dfd45740431f00d2f5fe2622f
Azazel_ <98657790+RAzazelo@users.noreply.github.com>
2023-10-14 22:27:21 +0200

    Update Main.php

--- AzazelFly/src/Azazel/Fly/Main.php
+++ AzazelFly/src/Azazel/Fly/Main.php
@@ -9,7 +9,12 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class Main extends PluginBase implements Listener{
+
+class FlyCommand extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
@@ -20,30 +25,26 @@ class Main extends PluginBase implements Listener{
         }
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if($command->getName() === "fly"){
-            if(!$sender instanceof Player){
-                $sender->sendMessage("Diesen Befehl kannst du nur im Spiel benutzen.");
-                return false;
-            }
-            if($sender->hasPermission("fly.command")){
-                if(!$sender->isCreative()){
-                    if(!$sender->getAllowFlight()){
-                        $sender->setAllowFlight(true);
-                        $sender->setFlying(true);
-                        $sender->sendMessage("§bServer §8» §7Dein Flugmodus wurde aktiviert.");
-                    }else{
-                        $sender->setAllowFlight(false);
-                        $sender->setFlying(false);
-                        $sender->sendMessage("§bServer §8»§c Dein Flugmodus wurde deaktiviert.");
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "fly") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("fly.command")) {
+                    if ($sender->getGamemode() !== 1) {
+                        if ($sender->getAllowFlight() === false) {
+                            $sender->setAllowFlight(true);
+                            $sender->sendMessage("§bServer §8» §7Dein Flugmodus wurde aktiviert.");
+                        } else {
+                            $sender->setAllowFlight(false);
+                            $sender->sendMessage("§bServer §8»§c Dein Flugmodus wurde deaktiviert.");
+                        }
+                    } else {
+                        $sender->sendMessage("§bServer §8»§c Dieser Befehl funktioniert nur im Überlebensmodus.");
                     }
-                }else{
-                    $sender->sendMessage("§bServer §8»§c Dieser Befehl funktioniert nur im Überlebensmodus.");
-                    return false;
+                } else {
+                    $sender->sendMessage("§bServer §8»§c Du hast nicht die benötigten Rechte! (fly.perms)");
                 }
-            }else{
-                $sender->sendMessage("§bServer §8»§c Du darst diesen Befehl nicht benutzen.");
-                return false;
+            } else {
+                $sender->sendMessage(TextFormat::RED . "AzazelFly » Dieser Befehl kann nur im Spiel verwendet werden.");
             }
         }
         return true;
